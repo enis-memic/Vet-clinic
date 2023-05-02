2a8b998335cb448ca3bcca516c76621efd4e26f6
Enis <enis.mema@gmail.com>
2023-05-02 10:45:47 +0200

    Add column species

--- schema.sql
+++ schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE animals (
 	date_of_birth DATE NOT NULL,
 	escape_attempts INTEGER UNIQUE NOT NULL,
 	neutered BOOLEAN NOT NULL,
-    weight_kg BIGINT NOT NULL
+    weight_kg BIGINT NOT NULL,
+	 species VARCHAR(50)
 );
