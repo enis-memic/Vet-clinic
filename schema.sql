2e15eda79889337f8c3ac47dd6dfd3f3bb423dc1
Enis <enis.mema@gmail.com>
2023-05-02 17:36:29 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -9,3 +9,35 @@ CREATE TABLE animals (
     weight_kg BIGINT NOT NULL,
 	 species VARCHAR(50)
 );
+
+-- Create new table -- 
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	full_name VARCHAR (50) NOT NULL,
+	age INT NOT NULL
+);
+
+-- Create new table -- 
+CREATE TABLE species(
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(50) NOT NULL
+);
+
+--Remove from animals table-- 
+ALTER TABLE animals
+  DROP COLUMN species;
+
+-- Add column species_id with foreign key to species table-- 
+ALTER TABLE animals ADD species_id INT REFERENCES species(id);
+
+ALTER TABLE animals
+ADD CONSTRAINT species_id
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+-- Add column owners_id with foreign key to owners table-- 
+ALTER TABLE animals ADD owner_id INT REFERENCES owners(id);
+ALTER TABLE animals
+ADD CONSTRAINT owner_id
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
